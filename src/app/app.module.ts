bbc286f62a17bde24fdcf4cbb47750bd3999992d
Alexius Lim <alexiuslim1994@gmail.com>
2023-11-30 23:21:30 +0800

    chore: configure prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,38 @@
+import { registerLocaleData } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import en from '@angular/common/locales/en';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterModule, provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
-import { CommonModule, registerLocaleData } from '@angular/common';
-import { appRoutes } from './app.routes';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
-import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
-import { IconsProviderModule } from './icons-provider.module';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IconsProviderModule } from './icons-provider.module';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
 registerLocaleData(en);
 
 @NgModule({
-  imports: [BrowserModule, NxWelcomeComponent, FormsModule, HttpClientModule, BrowserAnimationsModule, AppRoutingModule, IconsProviderModule, NzLayoutModule, NzMenuModule],
+  imports: [
+    BrowserModule,
+    NxWelcomeComponent,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    IconsProviderModule,
+    NzLayoutModule,
+    NzMenuModule,
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
   providers: [
     // provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
-    { provide: NZ_I18N, useValue: en_US }
+    { provide: NZ_I18N, useValue: en_US },
   ],
 })
 export class AppModule {}
