84020b102fee88bd6e9a7a43d4c21dd4317afe9d
Alexius Lim <alexiuslim1994@gmail.com>
2023-12-02 21:42:11 +0800

    chore: feat add ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import en from '@angular/common/locales/en';
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
@@ -27,12 +30,31 @@ registerLocaleData(en);
     IconsProviderModule,
     NzLayoutModule,
     NzMenuModule,
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true,
+        },
+      }
+    ),
+    EffectsModule.forRoot([]),
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true,
+        },
+      }
+    ),
+    StoreDevtoolsModule.instrument({ logOnly: !isDevMode() }),
   ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
-  providers: [
-    // provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
-    { provide: NZ_I18N, useValue: en_US },
-  ],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
 })
 export class AppModule {}
